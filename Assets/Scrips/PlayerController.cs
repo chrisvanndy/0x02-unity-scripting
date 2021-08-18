45591f9cfd83aad7766e0f0e92face1924445097
Chris Vanndy <2736@holbertonschool.com>
2021-08-18 16:11:29 -0500

    wrapping up task 9

--- Assets/Scrips/PlayerController.cs
+++ Assets/Scrips/PlayerController.cs
@@ -1,23 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour 
 {
+	private Scene scene;
 	public Rigidbody player;
 	private int score = 0;
+	public int health = 5;
 
 	public float speed = 2000f;
 	
 	// Use this for initialization
-	/*	void Start () 
+	void Start () 
 	{
-		
-	}*/
-	
+		scene = SceneManager.GetActiveScene();
+	}
+
   // Update is called once per frame
 	void Update () 
 	{
+		 if (health == 0)
+		 {
+			 Debug.Log("Game Over!");
+			 SceneManager.LoadScene(scene.name);
+		 }
 		 if (Input.GetKey("a") || Input.GetKey("left")) 
 		 {
 			  player.AddForce(-speed * Time.deltaTime, 0, 0);
@@ -46,6 +54,17 @@ public class PlayerController : MonoBehaviour
 				other.GetComponent<MeshRenderer>().enabled = false;
 				Debug.Log("Score: " + score);
 			}
+			if (other.GetComponent<Collider>().tag == "Trap")
+			{
+				health--;
+				Debug.Log("Health: " + health);
+			}
+
+			if (other.GetComponent<Collider>().tag == "Goal")
+			{
+				Debug.Log("You Win!");
+			}
+
 
 			// Debug.Log(other.GetComponent<Collider>().name);
 		}
